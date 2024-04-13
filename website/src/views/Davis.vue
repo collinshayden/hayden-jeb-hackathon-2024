6f44a43746e31ee6823766d5ef3cb57501638aaf
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2024-04-13 13:15:35 -0400

    added some stuff to Davis

--- website/src/views/Davis.vue
+++ website/src/views/Davis.vue
@@ -1,100 +1,110 @@
 <template>
-  <div class="davis">
-     <div class="body">
-       <iframe
-         src="https://maps.trilliumtransit.com/map/feed/ccta-vt-us/routes/19137?noui=true&page_embed=true"
-         width="100%"
-         height="400px"
-       ></iframe>
-       <div v-if="arrivals.length" v-for="stop in arrivals">
-        <h3>
-          {{ stop.stop_title }}
-        </h3>
-         <table class="fun-table">
-           <thead>
-             <tr>
-               <th>Route Name</th>
-               <th>Arrival Time</th>
-             </tr>
-           </thead>
-           <tbody>
-             <tr v-for="arrival in stop.data" :key="arrival.unixTime">
-               <td>{{ arrival.route_id }}</td>
-               <td>{{ arrival.formattedTime }}</td>
-             </tr>
-           </tbody>
-         </table>
-       </div>
-       <div v-else>Loading...</div>
-     </div>
-  </div>
- </template>
- 
- <script>
- export default {
-  name: "davis",
-  data() {
-     return {
-       arrivals: [],
-     };
-  },
-  created() {
-     // Fetch arrival times
-     fetch("/gtfsmap-realtime/feed/ccta-vt-us/arrivals?stopCode=805490")
-       .then((response) => response.json())
-       .then((data) => {
-         if (data.status === "success") {
-            this.arrivals.push(
-              {
-                stop_title: "UHeights Towards Williston", 
-                data: data.data
-              }
-            )
-         }
-       })
-       .catch((error) => console.error("Error fetching arrival times:", error));
-  },
-  // methods: {
-  //    getRouteName(routeId) {
-  //      return this.routeNames[routeId] || "Unknown";
-  //    },
-  // },
- };
- </script>
- 
+    <div class="davis">
+        <div class="body">
+            <iframe src="https://maps.trilliumtransit.com/map/feed/ccta-vt-us/routes/19137?noui=true&page_embed=true"
+                width="100%" height="400px"></iframe>
+            
+            <div v-if="arrivals.length" v-for="stop in arrivals">
+                <h3>
+                    {{ stop.stop_title }}
+                </h3>
+                <table class="fun-table">
+                    <thead>
+                        <tr>
+                            <th>Route Name</th>
+                            <th>Arrival Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="arrival in stop.data" :key="arrival.unixTime">
+                            <td>{{ arrival.route_id }}</td>
+                            <td>{{ arrival.formattedTime }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div v-else>Loading...</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "davis",
+    data() {
+        return {
+            arrivals: [],
+        };
+    },
+    created() {
+        // Fetch arrival times
+        fetch("/gtfsmap-realtime/feed/ccta-vt-us/arrivals?stopCode=805490")
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.status === "success") {
+                    this.arrivals.push(
+                        {
+                            stop_title: "UHeights Towards Williston",
+                            data: data.data
+                        }
+                    )
+                }
+            })
+            .catch((error) => console.error("Error fetching arrival times:", error));
+        fetch("/gtfsmap-realtime/feed/ccta-vt-us/arrivals?stopCode=805531")
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.status === "success") {
+                    this.arrivals.push(
+                        {
+                            stop_title: "UHeights Towards Downtown",
+                            data: data.data
+                        }
+                    )
+                }
+            })
+            .catch((error) => console.error("Error fetching arrival times:", error));
+    },
+    // methods: {
+    //    getRouteName(routeId) {
+    //      return this.routeNames[routeId] || "Unknown";
+    //    },
+    // },
+};
+</script>
+
 
 <style>
-
 .body {
-  width: 70%;
-  font-size: large;
-  padding: 10px;
-  margin: auto;
-  text-align: left;
+    width: 70%;
+    font-size: large;
+    padding: 10px;
+    margin: auto;
+    text-align: left;
 }
 
 html {
-  background-color: #b0cee8;
+    background-color: #b0cee8;
 }
 
 p {
-  font-size: 1.15em;
-  text-indent: 2em;
+    font-size: 1.15em;
+    text-indent: 2em;
 }
 
 table {
-  width: 100%;
-  border-collapse: collapse;
+    width: 100%;
+    border-collapse: collapse;
 }
 
 th,
 td {
-  border: 1px solid #ddd;
-  padding: 8px;
+    border: 1px solid #ddd;
+    padding: 8px;
 }
 
 th {
-  background-color: #f2f2f2;
+    background-color: #f2f2f2;
 }
 </style>
 <style scoped>
@@ -104,7 +114,8 @@ fun-table {
     font-family: Arial, sans-serif;
 }
 
-.fun-table th, .fun-table td {
+.fun-table th,
+.fun-table td {
     border: 1px solid #ddd;
     padding: 8px;
     text-align: left;
